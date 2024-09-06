f9ed4bfa5f1abf587038422a5a1022551885f779
latifismailadjie <latifismailadjie@gmail.com>
2024-09-06 17:39:44 +0000

    #bisa

--- config/database.js
+++ config/database.js
@@ -8,8 +8,9 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
-    port: process.env.DB_PORT
+    dialectModule: require('mysql2'),
   }
 );
 

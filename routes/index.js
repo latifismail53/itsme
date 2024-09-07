208f17e30de7b746d54eaa669b67b0564301f4e4
latifismail53 <latifismailadjie@gmail.com>
2024-09-07 16:51:21 +0700

    #updates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 const router = require("express").Router();
+const { name } = require("ejs");
 const pages = require("../controller/pages");
 router.get("/:name", pages.index);
 router.get("/", (req, res) => {
   res.json({
-    broh: "look it this brohhh!",
+    status: res.statusCode,
+    lastUpdatedAt: new Date() + "",
+    session: "valid",
+    message: "look it this brohhh!",
+    data: {
+      device: req.headers["user-agent"],
+      device_type: req.headers["user-agent"].split(" ")[0],
+      device_id: Math.floor(Math.random() * 99999999),
+    },
   });
 });
 module.exports = router;

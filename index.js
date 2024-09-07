6758b61db4890c0baeaf2ebe84cc3d699fbd17ab
latifismail53 <latifismailadjie@gmail.com>
2024-09-07 16:12:55 +0700

    #goks

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
-const db = require("./config/database");
 const express = require("express");
 const route = require("./routes");
+const path = require("path");
 const app = express();
 const port = parseInt(process.env.PORT) || process.argv[3] || 8080;
 
 app.use(route);
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Server run on port ${port}!`);
